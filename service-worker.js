82cf1363b0c1328ccd3f84f47f227449050ba694
Omar IBN CHAHDIA <>
2024-10-03 13:11:58 +0100

    add smart banner for android and ios

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+// Give your cache a name
+const CACHE_NAME = 'my-app-cache-v1';
+
+// List of files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/icon-192.png',
+  '/assets/icon-512.png'
+];
+
+// Install event - caching files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serving cached content when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
+
+// Activate event - updating the cache
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+  );
+});
