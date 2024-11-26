bbdf0b7b93de0d407656d43434e03b037c2c99e1
Omar IBN CHAHDIA <>
2024-11-27 00:00:53 +0100

    change #2

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,11 @@
-// Give your cache a name
-const CACHE_NAME = 'my-app-cache-v1';
+// service-worker.js
 
-// List of files to cache
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/assets/icon-192.png',
-  '/assets/icon-512.png'
-];
-
-// Install event - caching files
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing.');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  // You can add resources to cache here if needed
 });
 
-// Fetch event - serving cached content when offline
 self.addEventListener('fetch', (event) => {
   console.log('Fetching:', event.request.url);
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
-  );
-});
-
-// Activate event - updating the cache
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activate.');
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (!cacheWhitelist.includes(cacheName)) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-  );
+  // You can intercept network requests and serve cached resources here
 });
